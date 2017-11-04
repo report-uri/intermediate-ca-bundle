792f6983e4bbbabcb06cdb9aeeb8c391b79b8fc6
Scott Helme <scotthelme@hotmail.com>
2017-11-04 20:53:07 +0000

    Script to fetch intermediates.

---
+++ fetch-intermediates.php
@@ -0,0 +1,18 @@
+<?php
+
+$certs = array();
+$csv = file_get_contents('https://ccadb-public.secure.force.com/mozilla/PublicAllIntermediateCertsWithPEMCSV');
+$data = str_getcsv($csv);
+
+foreach($data as $item) {
+    $start = strpos($item, '-----BEGIN CERTIFICATE-----');
+    if($start !== false) {
+        $end = strpos($item, '-----END CERTIFICATE-----', $start) + 25;
+        $certs[] = substr($item, $start, $end - $start);
+    }
+}
+
+$text = implode("\r\n\r\n", $certs)."\r\n\r\n";
+file_put_contents('intermediate-bundle.crt', $text);
+
+?>

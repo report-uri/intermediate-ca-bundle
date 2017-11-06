617f4eb0568f2eeb67d7ea161f1127936ed9b02b
Aidan Woods <aidantwoods@gmail.com>
2017-11-06 18:45:23 +0000

    Cleanup function used for autoload

--- autoload.php
+++ autoload.php
@@ -1,18 +1,18 @@
 <?php
 declare(strict_types=1);
 
-function requireDir(string $dir)
-{
+$requireDir = function (string $dir) use (&$requireDir) {
     foreach (\scandir($dir) as $item) {
         if ($item === '.' || $item === '..') { continue; }
 
         $path = "$dir/$item";
 
-        if (\is_dir($path)) { \requireDir($path); }
+        if (\is_dir($path)) { $requireDir($path); }
         elseif (\is_file($path) || \substr($path, -4) === '.php') {
             require_once $path;
         }
     }
-}
+};
 
-\requireDir(__DIR__.'/src');
+$requireDir(__DIR__.'/src');
+unset($requireDir);

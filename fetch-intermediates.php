35b4ee2201665cd7743e7ea7bf13525b158fb201
Aidan Woods <aidantwoods@gmail.com>
2017-11-06 22:35:34 +0000

    Enable strict types and add some extra type and sanity checking

--- fetch-intermediates.php
+++ fetch-intermediates.php
@@ -1,16 +1,17 @@
 <?php
+declare(strict_types=1);
 
-$certs = array();
 $csv = file_get_contents('https://ccadb-public.secure.force.com/mozilla/PublicAllIntermediateCertsWithPEMCSV');
+if ($csv === false) { throw new \Exception("Could not get certificates"); }
+
 $data = str_getcsv($csv);
 
-foreach($data as $item) {
+$certs = array_filter(array_map(function(string $item) {
     $start = strpos($item, '-----BEGIN CERTIFICATE-----');
-    if($start !== false) {
-        $end = strpos($item, '-----END CERTIFICATE-----', $start) + 25;
-        $certs[] = substr($item, $start, $end - $start);
-    }
-}
+    if($start === false) { return null; }
+    $end = strpos($item, '-----END CERTIFICATE-----', $start) + 25;
+    return substr($item, $start, $end - $start);
+}, $data), function($item) : bool { return $item !== null; });
 
 $text = implode("\r\n\r\n", $certs)."\r\n\r\n";
 file_put_contents(__DIR__ . '/res/intermediate-ca-bundle.pem', $text);

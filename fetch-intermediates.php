2724cfdf017f59a0c974e4e6863fde5e3fe029d9
Aidan Woods <aidantwoods@gmail.com>
2017-11-06 23:40:44 +0000

    Codestyle

--- fetch-intermediates.php
+++ fetch-intermediates.php
@@ -2,16 +2,33 @@
 declare(strict_types=1);
 
 $csv = file_get_contents('https://ccadb-public.secure.force.com/mozilla/PublicAllIntermediateCertsWithPEMCSV');
-if ($csv === false) { throw new \Exception("Could not get certificates"); }
+if ($csv === false) {
+    throw new \Exception("Could not get certificates");
+}
 
 $data = str_getcsv($csv);
 
-$certs = array_filter(array_map(function(string $item) {
-    $start = strpos($item, '-----BEGIN CERTIFICATE-----');
-    if($start === false) { return null; }
-    $end = strpos($item, '-----END CERTIFICATE-----', $start) + 25;
-    return substr($item, $start, $end - $start);
-}, $data), function($item) : bool { return $item !== null; });
+$maybeCerts = array_map(
+    function (string $item) {
+        $start = strpos($item, '-----BEGIN CERTIFICATE-----');
+
+        if ($start === false) {
+            return null;
+        }
+
+        $end = strpos($item, '-----END CERTIFICATE-----', $start) + 25;
+
+        return substr($item, $start, $end - $start);
+    },
+    $data
+);
+
+$certs = array_filter(
+    $maybeCerts,
+    function($item) : bool {
+        return $item !== null;
+    }
+);
 
 $text = implode("\r\n\r\n", $certs)."\r\n\r\n";
 file_put_contents(__DIR__ . '/res/intermediate-ca-bundle.pem', $text);
